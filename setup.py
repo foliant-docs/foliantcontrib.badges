53a829ef63e09c03fae1698b209a15d5be36437f
minuhin <minuhin@restream.rt.ru>
2019-06-11 13:41:57 +0300

    add content type

--- setup.py
+++ setup.py
@@ -15,6 +15,7 @@ setup(
     name='foliantcontrib.badges',
     description=SHORT_DESCRIPTION,
     long_description=LONG_DESCRIPTION,
+    long_description_content_type='text/markdown',
     version='1.0.0',
     author='Daniil Minukhin',
     author_email='ddddsa@gmail.com',
